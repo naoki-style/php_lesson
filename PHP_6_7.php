cf3d7d251aad3b3339a995878c4296df7e2027c9
石井 直樹 <ishiinaoki@naoki-MBA.local>
2013-11-30 13:20:24 +0900

    Loops: While and Do-While - 7

---
+++ PHP_6_7.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<title>Your own do-while</title>
+        <link type='text/css' rel='stylesheet' href='style.css'/>
+	</head>
+	<body>
+    <?php
+        //write your do-while loop below
+        do {
+            echo 'inside loop';
+        } while(false);
+        echo 'outside loop';
+    ?>
+    </body>
+</html>
